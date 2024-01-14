e130456f265c8f7f44aa4ef523aab065b46b1a81
Kuroshiel <alifianfikry@gmail.com>
2024-01-14 17:08:15 +0700

    Learn Database Pooling

---
+++ database.go
@@ -0,0 +1,22 @@
+package belajargolangdatabase
+
+import (
+	"database/sql"
+	"time"
+)
+
+// Database Pooling
+
+func GetConnection() *sql.DB {
+	db, err := sql.Open("mysql", "root:Kuroshiel123A!@tcp(localhost:3306)/belajar_golang_database")
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxIdleTime(60 * time.Minute)
+
+	return db
+}
