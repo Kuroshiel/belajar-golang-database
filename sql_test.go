a5eebfca0e0936cd6bcf53f854b28982d5e9851e
Kuroshiel <alifianfikry@gmail.com>
2024-01-14 21:47:05 +0700

    Learn Ekeskusi Perintah SQL

---
+++ sql_test.go
@@ -0,0 +1,24 @@
+package belajargolangdatabase
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+// Ekeskusi Perintah SQL
+
+func TestExecSql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "INSERT INTO customer(id, name) VALUES ('joko', 'Joko')"
+	_, err := db.ExecContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Success insert new customer")
+}
